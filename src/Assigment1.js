5d6ee5bb24afb3bf286fdd40894720aa3a15d45c
Abhilash Kumar Vedwan <abhi211193@gmail.com>
2017-07-24 16:10:43 +0530

    <7/24/2017> Assignment: 1
    cosmetic: changed tabs to 4 spaces

--- src/Assigment1.js
+++ src/Assigment1.js
@@ -1,28 +1,27 @@
 // Printing Array elements with Comma delimiters
 function printArray(arr) {
-	return arr.join();
+    return arr.join();
 }
 
 // Opposite number
 function oppositeNumber(num) {
-	return -1 * num;
+    return -1 * num;
 }
 
 // Basic Mathematical Operations
 const op = {
-	'-': (a, b) => a - b,
-	'+': (a, b) => a + b,
-	'*': (a, b) => a * b,
-	'/': (a, b) => a / b
+    '-': (a, b) => a - b,
+    '+': (a, b) => a + b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b
 };
 function basicOp(operation, value1, value2) {
-	return op[operation](value1, value2);
+    return op[operation](value1, value2);
 }
 
 // Transportation on vacation
 const perDay = 40;
 const GE = (a, b) => a >= b;
 function rentalCarCost(d) {
-  return d * perDay - (GE(d, 7) && 50 || GE(d, 3) && 20 || 0);
+    return d * perDay - (GE(d, 7) && 50 || GE(d, 3) && 20 || 0);
 }
-
