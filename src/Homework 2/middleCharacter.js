c8e6330b60cc40487d76fc38a02f9ba5763e542d
Abhilash <abhi211193@gmail.com>
2017-07-25 20:14:38 +0530

    <7/25/2017> Assignment: 2
    more readable way to get the middle characters

--- src/Homework 2/middleCharacter.js
+++ src/Homework 2/middleCharacter.js
@@ -1,4 +1,4 @@
 function getMiddle(str) {
-    const n = str.length;
-    return n % 2 && str[(n - 1) / 2] || [str[n / 2 - 1], str[n / 2]].join('');
+    const n = Math.floor(str.length / 2);
+    return str.length % 2 && str[n] || str[n - 1].concat(str[n]);
 }
