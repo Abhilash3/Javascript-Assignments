fe579e09acd9dadac1cf00ffe5ba106c1a2501ce
Abhilash <abhi211193@gmail.com>
2017-07-25 20:00:47 +0530

    <7/25/2017> Assignment: 2
    simplifying number functions, fixing issues with result as 0

--- src/Homework 2/calculatingWithFunctions.js
+++ src/Homework 2/calculatingWithFunctions.js
@@ -1,18 +1,17 @@
-const defaultBehavior = (a, expression) => expression && expression(a) || a;
-const defaultScope = null;
+const template = a => (expression => !expression && a || expression(a));
 
-const zero = defaultBehavior.bind(defaultScope, 0);
-const one = defaultBehavior.bind(defaultScope, 1);
-const two = defaultBehavior.bind(defaultScope, 2);
-const three = defaultBehavior.bind(defaultScope, 3);
-const four = defaultBehavior.bind(defaultScope, 4);
-const five = defaultBehavior.bind(defaultScope, 5);
-const six = defaultBehavior.bind(defaultScope, 6);
-const seven = defaultBehavior.bind(defaultScope, 7);
-const eight = defaultBehavior.bind(defaultScope, 8);
-const nine = defaultBehavior.bind(defaultScope, 9);
+const zero = template(0);
+const one = template(1);
+const two = template(2);
+const three = template(3);
+const four = template(4);
+const five = template(5);
+const six = template(6);
+const seven = template(7);
+const eight = template(8);
+const nine = template(9);
 
-const plus = (a) => ((b) => b + a);
-const minus = (a) => ((b) => b - a);
-const times = (a) => ((b) => b * a);
-const dividedBy = (a) => ((b) => b / a);
+const plus = a => (b => b + a);
+const minus = a => (b => b - a);
+const times = a => (b => b * a);
+const dividedBy = a => (b => b / a);
