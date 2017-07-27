81c80c362555a1aa5a85c2c03591f5a9eeae53ff
Abhilash <abhi211193@gmail.com>
2017-07-27 19:35:01 +0530

    <7/27/2017> Assignment: 2
    fixing review comments

--- src/Homework 2/calculatingWithFunctions.js
+++ src/Homework 2/calculatingWithFunctions.js
@@ -1,17 +1,17 @@
-const template = a => expression => expression ? expression(a) : a;
+let template = a => expression => expression ? expression(a) : a;
 
-const zero = template(0);
-const one = template(1);
-const two = template(2);
-const three = template(3);
-const four = template(4);
-const five = template(5);
-const six = template(6);
-const seven = template(7);
-const eight = template(8);
-const nine = template(9);
+let zero = template(0);
+let one = template(1);
+let two = template(2);
+let three = template(3);
+let four = template(4);
+let five = template(5);
+let six = template(6);
+let seven = template(7);
+let eight = template(8);
+let nine = template(9);
 
-const plus = a => b => b + a;
-const minus = a => b => b - a;
-const times = a => b => b * a;
-const dividedBy = a => b => b / a;
+let plus = a => b => b + a;
+let minus = a => b => b - a;
+let times = a => b => b * a;
+let dividedBy = a => b => b / a;
--- src/Homework 2/middleCharacter.js
+++ src/Homework 2/middleCharacter.js
@@ -1,4 +1,4 @@
 function getMiddle(str) {
-    const n = Math.floor(str.length / 2);
+    let n = Math.floor(str.length / 2);
     return str.length % 2 && str[n] || str[n - 1].concat(str[n]);
 }
